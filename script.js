b8a7bc33dfac160343240de6c09437cc3c5d9a7b
Alix17 <wildflowerinabox@gmail.com>
2021-09-26 20:51:58 -0700

    psuedocode for javascript

---
+++ script.js
@@ -0,0 +1,14 @@
+//Assignment Code
+//Make a quiz
+//click start and a timer begins
+//Given a question
+//When first question answered, goes to next question next question
+//When answered incorrectly time is subtracted from timer countdown
+//Questions are multiple choice
+//5 questions in example
+//When all questions answered or timer reaches zero it is done
+//When game over you can save score
+//example had 75 seconds
+
+
+generateBtn.addEventListener 
