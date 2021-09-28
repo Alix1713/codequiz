9589f7cc1eb28fe6b82202c9e3f7da90c25da417
Alix17 <wildflowerinabox@gmail.com>
2021-09-28 00:03:01 -0700

    hitting my head against a wall of if statements

--- script.js
+++ script.js
@@ -10,17 +10,17 @@
 //When game over you can save score
 //example had 75 seconds
 
-//write start button
-var generateBtn = document.querySelector("#start");
-
-generateBtn.onclick = activate timer;
-
 //write 5 questions for the html box
 //5 questions : 4 answers for each question : buttons for each answer make buttons
 //turn each question into a function
 // make strings for answers to questions?
+//write start button
 
-let questions = [
+var generateBtn = document.querySelector("start");
+
+generateBtn.onclick =(){
+prompt (questions)
+ questions = [
     {
     numb: 1,
     question: "What does CSS stand for?",
@@ -48,15 +48,17 @@ let questions = [
             question: "What do you use psuedocode for?",
             answer: "adding notation",
             options: [
-              "adding images",
-              "adding notation",
-              "adding paragraphs",
-              "adding music" 
-            ]}
+              "adding images"===false;
+              "adding notation"===true;
+              "adding paragraphs"===false;
+              "adding music"===false;
+            ]}[
+            if answer ===true 
+            let questionsAnswer= [+1]}
               let questions = [
                 {
                 numb: 4,
-                question: "What is....",
+                question: "What is ...",
                 answer: "Cascading Style Sheet",
                 options: [
                   "Common Style Selector",
@@ -84,13 +86,26 @@ questionsAnswer.push ("correct")
 //TIMER?!!??!?
 setInterval(timer, 75) //set timer at 75 seconds
 function timer();{
-    {if (questionsAnswer ===false){
-    timer -=5; 
+    {if (questionsAnswer === false){
+    timer -5; 
 }
+var sec = 15;
+var time = setInterval(myTimer, 75);
 
 //input box for name
 
+
 //store score
+var points = 0;
+var total = [];
+
+//for loop from w3
+for (var i = 0; i < total; i++)
+{
+if (questions[i] == answers[i]) {
+        points = points + 1; 
+    }
+}
 
 //show score
 function show.results
